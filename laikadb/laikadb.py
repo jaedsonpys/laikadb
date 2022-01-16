e1d8b4deafd911e172bb5fced8f96a3e636be4e5
Jaedson Silva <contacanal798@gmail.com>
2022-01-16 01:20:33 -0300

    adicionando método para ler o documento do banco de dados.

--- laikadb/laikadb.py
+++ laikadb/laikadb.py
@@ -3,6 +3,9 @@ import os
 
 from datetime import datetime
 
+from exceptions import DBFileNotValid
+from exceptions import InvalidContentError
+
 DEFAULT_DB_STRUCTURE = {'isValid': None,
                         'createdIn': None,
                         'lastUpdate': None,
@@ -12,8 +15,7 @@ DEFAULT_DB_STRUCTURE = {'isValid': None,
 class LaikaDB:
     def __init__(
             self,
-            dbname: str = 'laika.db',
-            cryptography: bool = True
+            dbname: str = 'laika.db'
     ) -> object:
         """Inicializa ou conecta-se
         a um banco de dados.
@@ -25,16 +27,9 @@ class LaikaDB:
 
         :param dbname: Nome do documento do banco de dados.
         (o padrão é "laika.db").
-
-        :param cryptography: True para ativar a criptografia
-        e False para desativar.
         """
 
-        # criar método para criptografar dados
-
-        self.cryptography = cryptography
         self.dbname = dbname
-
         self._initialize()
 
     def _initialize(self) -> None:
@@ -49,8 +44,6 @@ class LaikaDB:
 
             return self._create_db()
 
-        self._open_db()
-
     def _create_db(self) -> None:
         time_now = str(datetime.now())
         db_dict: dict = DEFAULT_DB_STRUCTURE.copy()
@@ -62,3 +55,34 @@ class LaikaDB:
         with open(self.dbname, 'w') as db:
             json.dump(db_dict, db, indent=4, ensure_ascii=False)
 
+    def _open_db(self) -> dict:
+        """Abre e retorna o conteúdo
+        do banco de dados
+
+        :return: Conteúdo do banco de dados
+        """
+
+        try:
+            with open(self.dbname, 'r') as db:
+                db_data = json.load(db)
+        except FileNotFoundError:
+            raise FileNotFoundError('DB file not found')
+        except json.JSONDecodeError:
+            raise DBFileNotValid
+        else:
+            is_valid = db_data.get('isValid')
+            created_in = db_data.get('createdIn')
+            last_update = db_data.get('lastUpdate')
+            content = db_data.get('content')
+
+            list_for_verify = [is_valid, created_in,
+                               last_update, content]
+
+            # verificando se os valores existem
+            if not all(list_for_verify):
+                raise DBFileNotValid
+
+            if not isinstance(content, dict):
+                raise InvalidContentError
+
+            return db_data
