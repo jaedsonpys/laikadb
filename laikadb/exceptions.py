840411ea31cde67785710233dc30d25a3c7bd972
Jaedson Silva <contacanal798@gmail.com>
2022-01-16 01:20:55 -0300

    criando nova exceção "InvalidContentError".

--- laikadb/exceptions.py
+++ laikadb/exceptions.py
@@ -5,3 +5,8 @@ class Base(Exception):
 class DBFileNotValid(Base):
     def __init__(self):
         super().__init__('Arquivo de banco de dados inválido')
+
+
+class InvalidContentError(Base):
+    def __init__(self):
+        super().__init__('Conteúdo do banco de dados inválido')
