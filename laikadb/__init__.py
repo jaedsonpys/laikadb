e0d48bbc6eb79117221a69fcd4638991ecf41ddd
Jaedson Silva <contacanal798@gmail.com>
2022-01-16 02:15:24 -0300

    adicionando importações em __init__.py

--- laikadb/__init__.py
+++ laikadb/__init__.py
@@ -4,5 +4,10 @@
 # Este projeto é open-source, siga as
 # instruções da licença.
 
+from .laikadb import LaikaDB
+
+from .exceptions import InvalidContentError
+from .exceptions import DBFileNotValid
+
 __version__ = '1.0.0dev'
 __author__ = 'Jaedson Silva'
